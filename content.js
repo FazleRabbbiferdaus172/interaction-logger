c854cfd527b1a0a2e0e1abd2d959e5e508530e9e
FazleRabbbiferdaus172 <rabbi.ferdaus@brainstation-23.com>
2023-11-01 16:14:07 +0600

    major improvements to contest js downloaded json, json is now in a format of tour steps

--- content.js
+++ content.js
@@ -7,12 +7,61 @@ function captureInteraction(type, target) {
     key: target.key ? target.key : false,
     target: {
         classList: target.classList ? Array.from(target.classList) : [],
-        tagName: target.tagName,
         attributes: target.attributes ? Array.from(target.attributes).map(attr => ({ name: attr.name, value: attr.value })): []
 
-    }
+    },
+    tagName: target.tagName,
   });
 }
+
+function generateTourSteps(interactionData) {
+    const actions = [];
+    let pendingClick = null; // Track pending click event
+    let keydownValues = []; // Track keydown values
+    interactionData.forEach(item => {
+        if (item.type === 'click') {
+            if (item.tagName === "BUTTON") {
+                const editButtonClass = item.target.classList.includes("o_form_button_edit");
+                const hasNameAttribute = item.target.attributes.some(attr => attr.name === 'name');
+                if (editButtonClass) {
+                    actions.push({
+                        trigger: ".o_form_button_edit",
+                        auto: true,
+                        run: 'click'
+                        // run: `text ${combinedKeydownValues}`
+                    });
+                }
+            } 
+            else {
+                const hasFieldWidgetClass = item.target.classList.includes('o_field_widget');
+                const hasNameAttribute = item.target.attributes.some(attr => attr.name === 'name');
+                if (keydownValues.length > 0) {
+                    const combinedKeydownValues = keydownValues.join('');
+                    keydownValues = [];
+                    actions[actions.length - 1].run =  `text ${combinedKeydownValues}`;
+                }
+                if (hasFieldWidgetClass && hasNameAttribute) {
+                    const triggerSelector = `.o_field_char[name='${item.target.attributes.find(attr => attr.name === 'name').value}']`;
+                    actions.push({
+                        trigger: triggerSelector,
+                        extra_trigger: '.o_td_label',
+                        auto: true,
+                        run: 'click'
+                        // run: `text ${combinedKeydownValues}`
+                    });
+                }
+            }
+        } else if (item.type === 'keydown' & item.key !== 'Backspace') {
+            keydownValues.push(item.key);
+        }
+    });
+    if (keydownValues.length > 0) {
+        const combinedKeydownValues = keydownValues.join('');
+        keydownValues = [];
+        actions[actions.length - 1].run =  `text ${combinedKeydownValues}`;
+    }
+    return actions 
+  }
 
 function handleKeyDown(event) {
     // Clear the previous timer (if any)
@@ -22,7 +71,7 @@ function handleKeyDown(event) {
     typingTimer = setTimeout(function() {
         // Your code to handle the keydown event goes here
         captureInteraction('keydown', event);
-    }, 300); // Adjust the delay (in milliseconds) as needed
+    }, 100); // Adjust the delay (in milliseconds) as needed
 }
 
 function stopLogging() {
@@ -47,3 +96,29 @@ function startLogging() {
     console.log('started')
   }
   
+  function downloadLog() {
+    chrome.storage.local.get(['interactionData'], function(result) {
+      const interactions = result.interactionData || [];
+      actions = generateTourSteps(interactions);
+      let currentUrl = window.location.toString()
+      const urlObject = new URL(currentUrl);
+      const pathAndQuery = urlObject.pathname + urlObject.search + urlObject.hash;
+      tour_des = {
+        url: pathAndQuery,
+        sequence: 40,
+      }
+      tour = {tour_description: tour_des,
+              steps: actions
+        }
+      const jsonContent = JSON.stringify(tour, null, 2);
+      const blob = new Blob([jsonContent], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'interaction_data.json';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    });
+    chrome.storage.local.set({ interactionData: [] });
+  }
--- popup.js
+++ popup.js
@@ -10,21 +10,42 @@ document.getElementById('startLogging').addEventListener('click', function() {
       code: 'stopLogging()'
     });
   });
-  
+
   document.getElementById('downloadJson').addEventListener('click', function() {
-    chrome.storage.local.get(['interactionData'], function(result) {
-      const interactions = result.interactionData || [];
-      console.log(interactions)
-      const jsonContent = JSON.stringify(interactions, null, 2);
-      const blob = new Blob([jsonContent], { type: 'application/json' });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'interaction_data.json';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
+    chrome.tabs.executeScript({
+      code: 'downloadLog()'
     });
-    chrome.storage.local.set({ interactionData: [] });
   });
+
+  function generateTourSteps(interactionData) {
+    const actions = [];
+    let pendingClick = null; // Track pending click event
+    let keydownValues = []; // Track keydown values
+    interactionData.forEach(item => {
+        if (item.type === 'click') {
+            const hasFieldWidgetClass = item.target.classList.includes('o_field_widget');
+            const hasNameAttribute = item.target.attributes.some(attr => attr.name === 'name');
+    
+            if (hasFieldWidgetClass && hasNameAttribute) {
+                if (keydownValues.length > 0) {
+                    const combinedKeydownValues = keydownValues.join('');
+                    keydownValues = [];
+                    actions[actions.length - 1].run =  `text <${combinedKeydownValues}>`;
+                }
+                const triggerSelector = `.o_field_char[name="${item.target.attributes.find(attr => attr.name === 'name').value}"]`;
+                actions.push({
+                    trigger: triggerSelector,
+                    extra_trigger: '.o_td_label',
+                    auto: true,
+                    run: 'click'
+                    // run: `text <${combinedKeydownValues}>`
+                });
+            }
+        } else if (item.type === 'keydown' && pendingClick) {
+            keydownValues.push(item.key);
+        }
+    });
+    return actions 
+  }
+
   
